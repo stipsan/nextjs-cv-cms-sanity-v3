d96a653691e28c580ed89b5fe7dbef1991b9f719
Cody Olsen <stipsan@gmail.com>
2022-02-15 22:47:56 +0100

    move to sanity

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -7,10 +7,51 @@ import schemaTypes from 'all:part:@sanity/base/schema-type'
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
   // We name our schema
-  name: 'default',
+  name: 'CV',
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
-    /* Your types here! */
+    {
+      title: 'Company',
+      name: 'company',
+      type: 'document',
+      fields: [
+        {
+          title: 'Name',
+          name: 'name',
+          type: 'string',
+        },
+        {
+          title: 'Slug',
+          name: 'slug',
+          type: 'slug',
+          options: { source: 'name' },
+        },
+        {
+          title: 'LinkedIn',
+          name: 'linkedin',
+          type: 'url',
+        },
+        {
+          title: 'Logo',
+          name: 'logo',
+          type: 'image',
+          description:
+            'The logo will need render in a timeline that uses a CSS drop-shadow effect to create a white outline. Thus the opacity of the logo must be deliberate in order for it to look great. And generally square logos look better.',
+        },
+      ],
+    },
+    {
+      title: 'Experience',
+      name: 'experience',
+      type: 'document',
+      fields: [
+        {
+          title: 'Role',
+          name: 'role',
+          type: 'string',
+        },
+      ],
+    },
   ]),
 })

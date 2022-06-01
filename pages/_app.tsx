ed0cde7a27edb05b26f709b05906b142c8e67676
Cody Olsen <stipsan@gmail.com>
2022-06-01 23:29:37 +0200

    chore: bump deps

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,17 +8,15 @@ import { Toaster } from 'react-hot-toast'
 
 function App({ Component, pageProps }) {
   return (
-    <>
+    <NextIntlProvider
+      messages={pageProps.messages}
+      now={new Date(pageProps.now)}
+      timeZone="Europe/Oslo"
+    >
       <Toaster containerClassName="print:hidden" />
-      <NextIntlProvider
-        messages={pageProps.messages}
-        now={new Date(pageProps.now)}
-        timeZone="Europe/Oslo"
-      >
-        {process.env.NEXT_PUBLIC_COUNTER_DEV && <CounterSnippet />}
-        <Component {...pageProps} />
-      </NextIntlProvider>
-    </>
+      {process.env.NEXT_PUBLIC_COUNTER_DEV && <CounterSnippet />}
+      <Component {...pageProps} />
+    </NextIntlProvider>
   )
 }
 

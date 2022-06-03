06cd6c56eb0c7bfaf0b83699aed9c0f8dad27138
Cody Olsen <stipsan@gmail.com>
2022-06-03 02:37:29 +0200

    fix: remove next/dynamic wrapper

--- pages/studio/[...tool].tsx
+++ pages/studio/[...tool].tsx
@@ -1,8 +1,11 @@
 import dynamic from 'next/dynamic'
+import Studio from 'components/SanityStudio.client'
 
+/*
 const Studio = dynamic(() => import('components/SanityStudio.client'), {
   ssr: false,
 })
+// */
 
 export default function StudioPage() {
   return <Studio />

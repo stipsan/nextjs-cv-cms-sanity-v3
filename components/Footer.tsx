3ddbf25406dd87f1d47ac648468231d16cea9f89
Cody Olsen <stipsan@gmail.com>
2022-07-07 01:42:13 +0200

    chore: don't prefetch the CMS™

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -83,7 +83,7 @@ export default memo(function Footer({
           url="https://tailwindcss.com/"
         />
       </div>
-      <Link href="/studio" locale={false}>
+      <Link href="/studio" locale={false} prefetch={false}>
           <a className="inline-flex items-center justify-center mt-4 text-xs font-medium text-white bg-blue-600 border border-transparent rounded-full print:hidden hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
             <svg
               aria-label="Sanity"

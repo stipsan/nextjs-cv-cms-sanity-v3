e02f2d524ac0e4e9e9113f011f93ed3bc5fd57b8
Cody Olsen <stipsan@gmail.com>
2022-07-10 20:02:01 +0200

    fix Buffer check

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ const i18n = require('./intl.config.json')
  **/
 const nextConfig = {
   experimental: {
+    // urlImports: ['https://deno.land/x/'],
     browsersListForSwc: true,
     legacyBrowsers: false,
   },
@@ -16,6 +17,24 @@ const nextConfig = {
     domains: ['cdn.sanity.io', 'source.unsplash.com'],
     formats: ['image/avif', 'image/webp'],
   },
+  webpack: (config, { webpack }) => {
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        // To allow esbuild-wasm to run on the edge
+        'typeof Buffer': JSON.stringify('undefined'),
+      })
+    )
+
+    return config
+  },
+  /*
+  webpack: (config, options) => {
+    config.experiments = {
+      // topLevelAwait: true,
+    }
+    return config
+  },
+  // */
 }
 
 module.exports = nextConfig

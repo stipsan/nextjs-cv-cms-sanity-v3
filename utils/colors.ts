d5a43a59be33191792e1de64eaa080e334967d1d
Cody Olsen <stipsan@gmail.com>
2022-07-11 07:04:51 +0200

    Update colors.ts

--- utils/colors.ts
+++ utils/colors.ts
@@ -14,20 +14,3 @@ export const NEUTRAL_TONES = ['default', 'transparent']
 export const { blue, cyan, gray, green, magenta, orange, purple, red, yellow } =
   hues
 
-// https://github.com/sanity-io/design/blob/804bf73dffb1c0ecb1c2e6758135784502768bfe/packages/%40sanity/ui/src/theme/studioTheme/helpers.ts#L3-L17
-export function multiply(bg: string, fg: string): string {
-  const b = parseColor(bg)
-  const s = parseColor(fg)
-  const hex = rgbToHex(_multiply(b, s))
-
-  return hex
-}
-
-// https://github.com/sanity-io/design/blob/804bf73dffb1c0ecb1c2e6758135784502768bfe/packages/%40sanity/ui/src/theme/studioTheme/helpers.ts#L3-L17
-export function screen(bg: string, fg: string): string {
-  const b = parseColor(bg)
-  const s = parseColor(fg)
-  const hex = rgbToHex(_screen(b, s))
-
-  return hex
-}

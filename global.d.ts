782a8cebcc62ad365a93808537db6b3a75bd695c
Cody Olsen <81981+stipsan@users.noreply.github.com>
2022-11-26 13:17:36 +0100

    chore: WIP

--- global.d.ts
+++
@@ -1,6 +0,0 @@
-// Declaring this interface provides type safety for message keys
-type Messages = typeof import('./messages/en.json') &
-  ReturnType<
-    typeof import('./pages/internal/somecard')['getStaticProps']
-  >['props']['messages']['en']
-declare interface IntlMessages extends Messages {}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,7 @@ module.exports = {
   content: [
     './app/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
-    './hooks/**/*.{js,ts,jsx,tsx}',
-    './pages/**/*.{js,ts,jsx,tsx}',
+    './pages/api/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
